ca0235eb36fe59888e2cf965bb80510759943481
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2024-02-22 17:02:43 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-let myData = [
+[
     {
         name: "samara",
         job: "منظفة",
@@ -12,48 +12,3 @@ let myData = [
     },
 
 ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Get the container element where you want to append the data
-const container = document.querySelector(".data");
-
-// Iterate over the myData array
-myData.forEach((dataItem, index) => {
-    // Create a new div element for each data item
-    const div = document.createElement("div");
-
-    // Set the inner HTML of the div with the data from the current data item
-    div.innerHTML = `
-        <p>name${index}: ${dataItem.name}</p>
-        <p>job ${index}: ${dataItem.job}</p>
-        <p>image ${index} ${dataItem.image}</p>
-    `;
-
-    // Append the div to the container
-    container.appendChild(div);
-});
